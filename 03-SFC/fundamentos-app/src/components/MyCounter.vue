9722ba1e87f4ec13c2d5407e8dbba3febc36bbba
José Luis González <joseluisgonsan@gmail.com>
2022-08-03 15:59:19 +0200

    50. Incrementar y Decrementar

--- 03-SFC/fundamentos-app/src/components/MyCounter.vue
+++ 03-SFC/fundamentos-app/src/components/MyCounter.vue
@@ -1,7 +1,10 @@
 <template>
   <h2>Counter</h2>
-  <p>{{ counter }} <sup>2</sup> = {{ squareMethods() }}</p>
-  <p>{{ counter }} <sup>2</sup> = {{ counterSquare }}</p>
+  <p class="my-style">{{ counter }} <sup>2</sup> = {{ counterSquare }}</p>
+  <div>
+    <button @click="increment()">+1</button>
+    <button @click="decrement()">-1</button>
+  </div>
 </template>
 
 <script>
@@ -18,6 +21,12 @@
       squareMethods() {
         return this.counter * this.counter
       },
+      increment() {
+        this.counter++
+      },
+      decrement() {
+        this.counter--
+      },
     },
     // Mis propiedades computadas, se gaurdan en cache y solo cambia cuando cambia el estado que computan..., solo son de lectura
     computed: {
@@ -28,4 +37,22 @@
   }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+  h2 {
+    color: blueviolet;
+  }
+  button {
+    background-color: #64bb87;
+    border-radius: 5px;
+    color: white;
+    margin: 0 5px;
+    cursor: pointer;
+    padding: 5px 10px;
+    transition: 0.3s ease-in-out;
+  }
+  .my-style {
+    color: #444444;
+    font-size: 1.2em;
+    font-weight: bold;
+  }
+</style>
