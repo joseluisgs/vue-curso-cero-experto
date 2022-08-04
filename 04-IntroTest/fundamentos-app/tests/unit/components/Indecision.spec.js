b7ab432fd920e31919b0a0b126361cd326ad4d2b
José Luis González <joseluisgonsan@gmail.com>
2022-08-04 09:44:39 +0200

    80. Probar que el getAnswer fue llamado

--- 04-IntroTest/fundamentos-app/tests/unit/components/Indecision.spec.js
+++ 04-IntroTest/fundamentos-app/tests/unit/components/Indecision.spec.js
@@ -1,4 +1,4 @@
-import { describe, test, expect, beforeEach, vi } from 'vitest'
+import { describe, test, expect, beforeEach, vi, afterEach } from 'vitest'
 
 import { shallowMount } from '@vue/test-utils'
 import Indecision from '@/components/Indecision.vue'
@@ -11,6 +11,11 @@ describe('Component -> Indecision', () => {
   beforeEach(() => {
     wrapper = shallowMount(Indecision)
     clgSpy = vi.spyOn(console, 'log') // Vamos a espiar el console.log
+  })
+
+  afterEach(() => {
+    // Limpiamos todos los mocks!!
+    vi.resetAllMocks()
   })
 
   // cada una de las pruebas o test
@@ -32,12 +37,19 @@ describe('Component -> Indecision', () => {
 
     const input = wrapper.find('input')
     await input.setValue('testing')
-    expect(clgSpy).toHaveBeenCalledTimes(1) // Comprobamos que se ha llamado una vez
+    expect(clgSpy).toHaveBeenCalledTimes(1) // Comprobamos que se ha llamado una vez... Es una chapuza si tenemos una variable usar el console log ;)
     expect(getAnswerSpy).not.toHaveBeenCalled() // Comprobamos que no se ha llamado a la funcion getAnswer
     expect(isValidQuestion).toBe(false) // Comprobamos que la pregunta no es valida
   })
 
-  test('Al escribir ? debe disparar el el getAnswer', () => {})
+  test('Al escribir ? debe disparar el getAnswer', async () => {
+    // Simulamos el comprtamiento al ser llamdo para evitar que llame a fetch que no existe en node
+    const getAnswerSpy = vi.spyOn(wrapper.vm, 'getAnswer').mockImplementation(() => null)
+
+    const input = wrapper.find('input')
+    await input.setValue('testing?')
+    expect(getAnswerSpy).toHaveBeenCalled()
+  })
 
   test('Pruebas getAnswer', () => {})
 
