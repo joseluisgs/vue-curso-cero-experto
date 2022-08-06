98aa61e8361715dba4b429edc0adc2c92ef544bb
JoséLuis González Sánchez <joseluisgonsan@gmail.com>
2022-08-06 18:46:28 +0200

    Tema 07 init y Refactor

---
+++ 07-VueRouter-CicloVida/rutas-ciclo/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink> &nbsp; <RouterLink to="/about">About</RouterLink> &nbsp;
+    <!-- Si quiero usar el nombre de las vistas y no su url-->
+    <RouterLink :to="{ name: 'home' }">Home Named</RouterLink> &nbsp;
+  </nav>
+  <!-- Si quiero cachear las vistas usar keep alive 
+  <router-view v-slot="{ Component }">
+  <keep-alive>
+    <component :is="Component" />
+  </keep-alive>
+</router-view>
+-->
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ 07-VueRouter-CicloVida/rutas-ciclo/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>This is an home page</h1>
+</template>
+
+<style></style>
