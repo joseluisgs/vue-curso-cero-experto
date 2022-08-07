38ab553765bdc99bb886d157c5cb45d0ddbfd61d
JoséLuis González Sánchez <joseluisgonsan@gmail.com>
2022-08-07 19:57:16 +0200

    128. Petición HTTP y redirecciones

--- 07-VueRouter-CicloVida/rutas-ciclo/src/App.vue
+++ 07-VueRouter-CicloVida/rutas-ciclo/src/App.vue
@@ -8,7 +8,6 @@
   </keep-alive>
 </router-view>
 -->
-
   <RouterView />
 </template>
 
--- 07-VueRouter-CicloVida/rutas-ciclo/src/modules/pokemon/views/PokemonPage.vue
+++ 07-VueRouter-CicloVida/rutas-ciclo/src/modules/pokemon/views/PokemonPage.vue
@@ -4,12 +4,20 @@
   <h4>FullPath: {{ route.fullPath }}</h4>
   <h4>Param Id: {{ route.params.id }}</h4>
   <h4>Query: {{ route.query }}</h4>
+  <div v-if="pokemon">
+    <h4 v-if="pokemon">Pokemon Name: {{ pokemon.name }}</h4>
+    <img
+      :src="pokemon.sprites.front_default"
+      :alt="pokemon.name"
+    />
+  </div>
 
   <button @click="goHome()">Go Home</button>
 </template>
 
 <script setup>
   import { useRouter, useRoute } from 'vue-router'
+  import { ref, watch } from 'vue'
 
   // Vamos a pasarle unas propiedades!!!
   const props = defineProps({
@@ -23,6 +31,9 @@
   const router = useRouter()
   const route = useRoute()
 
+  // Mi pokemon
+  const pokemon = ref(null)
+
   console.log('Mi propiedad', props.id)
 
   console.log('Mi path', route.fullPath)
@@ -34,4 +45,33 @@
     // o usando nombraods
     router.push({ name: 'pokemon-list' })
   }
+
+  const go404 = () => {
+    router.push({ name: '404' })
+  }
+
+  const apiFetch = async () => {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${props.id}`)
+      const data = await response.json()
+      return data
+    } catch (err) {
+      console.log(err)
+      go404()
+    }
+  }
+
+  // onMounted(async () => {
+  //   pokemon.value = await apiFetch()
+  // })
+  // onUpdated(async () => {
+  //   pokemon.value = await apiFetch()
+  // })
+  // Watcher sobre una prop en compistion API: https://stackoverflow.com/questions/59125857/how-to-watch-props-change-with-vue-composition-api-vue-3
+  watch(
+    () => props.id,
+    async () => {
+      pokemon.value = await apiFetch()
+    }
+  )
 </script>
