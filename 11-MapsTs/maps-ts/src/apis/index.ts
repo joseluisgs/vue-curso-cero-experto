e6570fb5c398ecfeb2648e4e58e6a155576a7b29
JoséLuis González Sánchez <joseluisgonsan@gmail.com>
2022-12-05 10:15:00 +0100

    348. Obtener ruta entre dos puntos

--- 11-MapsTs/maps-ts/src/apis/index.ts
+++ 11-MapsTs/maps-ts/src/apis/index.ts
@@ -12,4 +12,15 @@ const searchApi = axios.create({
   },
 })
 
-export { searchApi }
+const directionsApi = axios.create({
+  baseURL: 'https://api.mapbox.com/directions/v5/mapbox/driving',
+  params: {
+    alternatives: false,
+    geometries: 'geojson',
+    overview: 'simplified',
+    steps: false,
+    access_token: import.meta.env.VITE_MAP_TOKEN,
+  },
+})
+
+export { searchApi, directionsApi }
